4f8be779fd67599be34e0452db5475fed0ccf6dd
BYRONTOSH <by_tosh20@hotmail.com>
2021-01-12 15:17:18 -0500

    ejercicio-calculadora-py

---
+++ calculadora.py
@@ -0,0 +1,29 @@
+
+
+print (2+2)
+
+print (50 - 5*6)
+
+print (50 - 5*6 / 4 )
+
+print (8/5)
+
+print (10//6)
+
+
+# DIVISIONES
+print("---------------")
+print(17/3)
+print(17//3)
+print(17%3)
+
+# POTENCIAS
+print("---------------")
+print(5**2)
+
+# VARIABLES 
+
+print("---------------")
+ancho =20
+largo =5
+print(ancho*largo)
